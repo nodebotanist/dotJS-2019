cfdcc71e06b39f744644e38923bb41b51bb7cb5a
nodebotanist <the@nodebotani.st>
2019-10-28 14:00:04 -0500

    updated index to match lib changes

--- band-code/controller/index.js
+++ band-code/controller/index.js
@@ -1,22 +1,6 @@
 const Launchpad = require("./node-launchpad-mk2/src/index")
-const midi = require('midi')
 
-// get Midi port for novation pad
-const input = new midi.Input()
-const ports = input.getPortCount()
-let launchpadPort = 0;
-
-for(let i=0; i<ports; i++) {
-	if(input.getPortName(i) === 'Launchpad MK2') {
-		launchpadPort = i
-		console.log(`Launchpad found at port ${i}`)
-	}
-}
-
-let myLaunchpad = new Launchpad({
-	in: launchpadPort,
-	out: launchpadPort
-})
+let myLaunchpad = new Launchpad({})
 
 myLaunchpad.on("press", pressInfo => {
 	if(pressInfo.button) console.log(pressInfo.button.note, pressInfo.button.x, pressInfo.button.y, pressInfo.velocity)
