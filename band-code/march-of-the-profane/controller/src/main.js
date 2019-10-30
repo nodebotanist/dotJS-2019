88910c84429a86d8f94360af226377b7201021d3
nodebotanist <the@nodebotani.st>
2019-10-30 15:53:49 -0500

    Basic opening measure down

--- band-code/march-of-the-profane/controller/src/main.js
+++ band-code/march-of-the-profane/controller/src/main.js
@@ -1,11 +1,38 @@
 const Tone = require('tone')
 //create a synth and connect it to the master output (your speakers)
-console.log(document.querySelector('#start'))
-document.querySelector('#start').addEventListener('click', () => {
-	console.log('playing note')
-	Tone.context.resume();
-	var synth = new Tone.Synth().toMaster();
+let synth = new Tone.Synth({
+	volume: 0
+}).toMaster();
 
-	//play a middle 'C' for the duration of an 8th note
-	synth.triggerAttackRelease("C4", "8n");
+document.querySelector('#start').addEventListener('click', () => {
+	Tone.context.resume();
+	Tone.Transport.bpm.value = 145 * 2
+	Tone.Transport.timeSignature = 6
+	// create an array of notes to be played
+	const notes = ["A#1", [null], null, null, null, [null, "C2"], "B#1", [[null]], "B1", "B1", null, null, null, [null, "B1"],null, [null, "B1"] ];
+	// create a new sequence with the synth and notes
+	const synthPart = new Tone.Sequence(
+		function (time, note) {
+			synth.triggerAttackRelease(note, "10hz", time);
+		},
+		notes,
+		"4n"
+	);
+	// Setup the synth to be ready to play on beat 1
+	synthPart.start();
+	Tone.Transport.start();
 })
+
+function beginningMeasures() {
+		Tone.context.resume();
+		synth.triggerAttackRelease("A#2", "16n");
+		// synth.volume.value = 0
+		synth.triggerAttackRelease("C4", "32n");
+		synth.triggerAttackRelease("C4", "16n");
+		synth.triggerAttackRelease("C4", "16n");
+		// synth.volume.value = 4
+		synth.triggerAttackRelease("C2", "16n");
+		synth.triggerAttackRelease("B#2", "8n");
+		synth.triggerAttackRelease("B2", "16n");
+		synth.triggerAttackRelease("B2", "8n");
+}
