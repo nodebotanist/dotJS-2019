1daae40dfa0d9d4d29e3087dc2d58bd511a24da0
Kassian Wren <the@nodebotani.st>
2019-11-25 17:33:42 +0000

    Added multiplier and hue logic

--- band-code/bass/index.js
+++ band-code/bass/index.js
@@ -1,5 +1,7 @@
 const midi = require('midi')
 const tonal = require('tonal')
+const color = require('color')
+
 const OPC = require('./opc')
 
 // Set up a new input.
@@ -48,5 +50,21 @@ setInterval(() => {
   }
 }, 200)
 
+setInterval(() => {
+  if(currentNote) {
+    noteToHue(currentNote)
+  }
+}, 200)
+
+function noteToHue(note) {
+  const noteArray = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+  let multiplier = noteArray.indexOf(note.charAt(0)) + 1
+  if(note.charAt(1) === 'b') {
+    multiplier -= 0.5
+  }
+  console.log(multiplier)
+  let hue = (256 / 8) * multiplier
+  let currentColor = color({h: hue, s: 100, l:50})
+}
 // Open the first available input port.
 input.openPort(port);
