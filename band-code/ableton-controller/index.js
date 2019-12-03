619c959e073b4591754b677599a7bf8cee44f517
nodebotanist <the@nodebotani.st>
2019-12-03 09:42:53 -0600

    Added WARK button to controller

--- band-code/ableton-controller/index.js
+++ band-code/ableton-controller/index.js
@@ -1,16 +1,21 @@
 const WebSocket = require('ws')
+const { createAudio } = require('node-mp3-player')
 
 const Launchpad = require("./node-launchpad-mk2/src/index")
 
 const maestroSocket = new WebSocket('ws://localhost:8081')
 
-maestroSocket.on('open', () => {
+const Audio = createAudio()
+let wark
+
+maestroSocket.on('open', async () => {
+	wark = await Audio(`${__dirname}/sounds/wark.mp3`)
 	console.log('maestro socket open')
 })
 
 let myLaunchpad = new Launchpad({})
 
-myLaunchpad.on("press", pressInfo => {
+myLaunchpad.on("press", async pressInfo => {
 	if(pressInfo.button) console.log(pressInfo.button.note, pressInfo.button.x, pressInfo.button.y, pressInfo.velocity)
 	else console.log(pressInfo)
 	if(pressInfo.button && pressInfo.button.note === 69) {
@@ -19,6 +24,9 @@ myLaunchpad.on("press", pressInfo => {
 	} else if (pressInfo.button && pressInfo.button.note === 49) {
 		console.log('stopping song')
 		maestroSocket.send('stopSong')
+	} else if (pressInfo.button && pressInfo.button.note === 73) {
+		console.log('playing wark!')
+		playSound('wark')
 	}
 })
 
@@ -29,3 +37,8 @@ myLaunchpad.on("release", button => {
 myLaunchpad.buttons.forEach((button) => {
 	button.setColor(button.note)
 })
+
+async function playSound(name) {
+	await wark.volume(1)
+	await wark.play()
+}
